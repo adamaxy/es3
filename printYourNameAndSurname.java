35bcc246845a2ddde6ba9a7a0aa166a89d0991de
adamaxy <138890873+adamaxy@users.noreply.github.com>
2023-07-21 10:45:32 +0200

    Update and rename printYourName.java to printYourNameAndSurname.java

--- printYourNameAndSurname.java
+++ printYourNameAndSurname.java
@@ -1,4 +1,4 @@
-public class printYourName {
+public class printYourNameAndSurname {
     public static void main( String [] args){
         System.out.println("giovanni ");
          System.out.println("cassano");
