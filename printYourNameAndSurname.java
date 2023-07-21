6169a211dbe35523408fc98b4a18e23d22c67e49
adamaxy <138890873+adamaxy@users.noreply.github.com>
2023-07-21 14:04:34 +0200

    Update printYourNameAndSurname.java

--- printYourNameAndSurname.java
+++ printYourNameAndSurname.java
@@ -1,6 +1,6 @@
 public class printYourNameAndSurname {
     public static void main( String [] args){
-        System.out.println("giovanni ");
-         System.out.println("cassano");
+        System.out.print("giovanni ");
+         System.out.print("cassano");
     }
 }
